cab80304c5cea24dac4041b00cc54569bf7fd546
zhangjie <zhangjie@choosefine.com>
2017-09-16 14:43:34 +0800

    修复没有生产者的时候，报application.yml配置文件中一定要指定rocketmq producer-group的错误

--- src/main/java/com/maihaoche/starter/mq/config/MQProducerAutoConfiguration.java
+++ src/main/java/com/maihaoche/starter/mq/config/MQProducerAutoConfiguration.java
@@ -17,7 +17,7 @@ import java.util.Map;
  */
 @Slf4j
 @Configuration
-@ConditionalOnBean(MQBaseAutoConfiguration.class)
+@ConditionalOnBean(value = MQBaseAutoConfiguration.class,annotation = MQProducer.class)
 public class MQProducerAutoConfiguration extends MQBaseAutoConfiguration {
 
     private DefaultMQProducer producer;
@@ -25,9 +25,6 @@ public class MQProducerAutoConfiguration extends MQBaseAutoConfiguration {
     @PostConstruct
     public void init() throws Exception {
         if(producer == null) {
-//            if(StringUtils.isEmpty(mqProperties.getProducerGroup())) {
-//                throw new RuntimeException("请在配置文件中指定消息发送方group！");
-//            }
             if(StringUtils.isEmpty(mqProperties.getProducerGroup())) {
                 throw new RuntimeException("producer group must be defined");
             }
